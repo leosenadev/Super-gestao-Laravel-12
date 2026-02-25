f414e42b0c0ebd3046c91cb4b5644baf7710bc19
leosenadev <leosenadev@gmail.com>
2026-02-25 11:45:02 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [PrincipalController::class,'principal'])->name('site.index');
+Route::get('/sobre-nos',[SobreNosController::class,'sobreNos'])->name('site.sobrenos');
+Route::get('/contato',[ContatoController::class,'contato'])->name('site.contato');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes',function(){ return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores',[FornecedorController::class,'index'])->name('app.fornecedores');
+    Route::get('/produtos',function(){ return 'Produtos';})->name('app.produtos');
+});
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe . <a href="'.route('site.index').'">Clique aqui </a> para ir para página inicial';
+});
